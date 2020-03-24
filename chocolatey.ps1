dac60ccd607e4a2ca80a15287f72b31cd0f00cd1
matheanswer <36707250+matheanswer@users.noreply.github.com>
2020-03-24 14:58:48 +0100

    Update chocolatey.ps1

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,1 +1,12 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Install Chocolatey
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Update Chocolatey and all packages
+choco upgrade all
+
+# Do more stuff
+
+# Wait for a key press to exit the script
+Write-Host ""
+Write-Host "Press a key to exit ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
